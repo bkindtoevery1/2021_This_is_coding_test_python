b8a7f06a71f02d9846f6d789b4065ae070e2fe17
chayhyeon <bkindtoevery1@gist.ac.kr>
2021-05-11 22:51:24 +0900

    solve '게임 개발'

---
+++ chap.04/game_develop.py
@@ -0,0 +1,28 @@
+n, m = tuple(map(int, input().split(' ')))
+x, y, d = tuple(map(int, input().split(' ')))
+game_map = []
+dx = [0, 1, 0, -1]
+dy = [-1, 0, 1, 0]
+for i in range(n):
+	game_map.append(list(map(int, input().split(' '))))
+ans = 0
+while True:
+	if game_map[x][y] == 0:
+		ans += 1
+		game_map[x][y] = -1
+	for i in range(5):
+		if i == 4:
+			d = (d + 2) % 4
+			if game_map[x + dx[d]][y + dy[d]] == 1:
+				print(ans)
+				exit(0)
+			else:
+				break
+		d = (d + (3 - i)) % 4
+		if not game_map[x + dx[d]][y + dy[d]]:
+			break
+	x += dx[d]
+	y += dy[d]
+
+	
+
